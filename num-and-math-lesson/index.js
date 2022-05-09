f3f8c0a5f4148814192ff5eb8f0d40df228216ad
Allan-Bruno <allanbrunooliveiravasconcelos@gmail.com>
2022-05-09 14:25:08 -0300

    hotfix

--- num-and-math-lesson/index.js
+++ num-and-math-lesson/index.js
@@ -4,9 +4,9 @@ const text = document.getElementById("text");
 
 titleNumber.innerHTML = number;
 
-text.innerHTML = `<p>A raiz quadrada desse número é: ${number ** 0.5}</p><br />
-<p>${number} é um inteiro?:${Number.isInteger(number)}</p><br />
-<p>${number} é um NaN?: ${Number.isNaN(number)}</p><br />
-<p>${number} arrendondado para baixo é: ${Math.floor(number)}</p><br />
-<p>${number} arrendondado para cima é: ${Math.ceil(number)}</p><br />
-<p>${number} com duas casas decimais é: ${number.toFixed(2)}</p><br />`;
+text.innerHTML = `<p>A raiz quadrada desse número é: ${number ** 0.5}</p>
+<p>${number} é um inteiro?:${Number.isInteger(number)}</p>
+<p>${number} é um NaN?: ${Number.isNaN(number)}</p>
+<p>${number} arrendondado para baixo é: ${Math.floor(number)}</p>
+<p>${number} arrendondado para cima é: ${Math.ceil(number)}</p>
+<p>${number} com duas casas decimais é: ${number.toFixed(2)}</p>`;
